60a56bc5bf6ca6367c53f22c8accfc4f00aab860
Melissa Aguirre <aguirregalvismelissa@gmail.com>
2022-12-06 15:52:16 -0500

    Modificaciones realizadas al ejercicio año bisiesto

--- ejercicio2/ejercicio_bisiesto.py
+++ ejercicio2/ejercicio_bisiesto.py
@@ -1,13 +1,14 @@
 def anio_bisiesto(anio):
     if anio % 4 == 0 and anio % 400 == 0 and anio % 100 == 0:
-        print ("Es un año bisiesto.") 
-    elif anio % 4 == 0 and anio % 400 == 0 and anio % 100 != 0:
-        print ("Es un año bisiesto.")
+        msj = ("Es un año bisiesto.") 
+    elif anio % 4 == 0 and anio % 400 != 0 and anio % 100 != 0:
+        msj = ("Es un año bisiesto.")
     elif anio % 4 == 0 and anio % 400 != 0 and anio % 100 == 0:
-        print ("No es un año bisiesto.")
+        msj = ("No es un año bisiesto.")
     elif anio % 4 != 0 and anio % 400 != 0 and anio % 100 != 0:
-        print ("No es un año bisiesto.")
+        msj = ("No es un año bisiesto.")
         
+    return msj     
 
 anio = (input("Ingresa un año: "))
 print (anio_bisiesto(anio))
